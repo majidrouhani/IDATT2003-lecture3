39f7cb736ca99ef5bcbdde56a56e104c3f8e55b6
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2022-12-16 12:53:09 +0100

    initial version

---
+++ src/main/java/edu/ntnu/idatt2001/lectures/App.java
@@ -0,0 +1,18 @@
+package edu.ntnu.idatt2001.lectures;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+  private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.log(Level.INFO,"Hello world!");
+    }
+}
